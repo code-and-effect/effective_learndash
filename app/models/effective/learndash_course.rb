43b73d4fe586432a08fdc8374a2b0a5ebc113827
Dana Janssen <dana@codeandeffect.com>
2026-08-26 10:03:54 -0600

    Use shared pagination normalization

--- app/models/effective/learndash_course.rb
+++ app/models/effective/learndash_course.rb
@@ -56,7 +56,7 @@ module Effective
     scope :published, -> { all }
 
     scope :paginate, -> (page: nil, per_page: nil) {
-      page = (page || 1).to_i
+      page = EffectiveResources.normalize_page(page)
       offset = [(page - 1), 0].max * (per_page || EffectiveLearndash.per_page)
 
       limit(per_page).offset(offset)
